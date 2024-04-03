a30ded47ef26371d9bab5862a418b88d9ab15925
Manuel Martin <manmartin@mozilla.com>
2024-04-03 11:27:21 +0200

    Remove RPM/DEB packagers

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
 import { WebpackPlugin } from "@electron-forge/plugin-webpack";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
@@ -21,8 +19,6 @@ const config: ForgeConfig = {
   makers: [
     new MakerSquirrel({}),
     new MakerZIP({}, ["darwin", "linux", "win32"]),
-    new MakerRpm({ options: { icon: "./static/icon_sunset" } }),
-    new MakerDeb({ options: { icon: "./static/icon_sunset" } }),
   ],
   plugins: [
     new AutoUnpackNativesPlugin({}),
